9f88ffbc1af3fa9e3a7fa559b15a8e6ea32676cc
bksim <simpavid@gmail.com>
2013-10-10 04:40:52 -0400

    working walksat, working improved walksat

--- asst3/diagnosis.py
+++ asst3/diagnosis.py
@@ -7,6 +7,7 @@ CS 182
 
 import random
 from random import choice
+import time
 
 # updates all literals in all clauses
 def updateClauses(clauses, literal_dict):
@@ -83,6 +84,66 @@ def WalkSAT(clauses, p, max_flips):
             clauses = updateClauses(clauses, literal_dict)
     return False
 
+# better "walksat" algorithm,
+# essentially just greedy algorithm
+def WalkSAT_Better(clauses, max_flips):
+    """YOUR CODE HERE"""
+    literal_dict = {}
+    for c in clauses:
+        for lit in c.literals:
+            literal_dict[lit.name] = False
+
+    # assign everything to false at the beginning
+    clauses = updateClauses(clauses, literal_dict)
+    numClauses = len(clauses)
+
+    #for i = 1 to max_flips:
+    for i in range(max_flips):
+        # for max count in randomizer later
+        notSatisfiedClauses = []
+
+        for j in range(numClauses):
+            if clauses[j].evaluate() == False:
+                notSatisfiedClauses.append(j)
+
+        numNotSatisfied = len(notSatisfiedClauses)
+
+        if numNotSatisfied == 0:
+            return clauses
+
+        # greedily chooses the best literals to flip in all the clauses instead of just a random clause
+        bestClause = -1 # best clause index
+        bestClause_numSatisfied = -1 # number satisfied in best clause
+        indexOfBestClause = -1 # index of the literal to change in the best clause
+
+        for cindex in notSatisfiedClauses:
+            literals = clauses[cindex].literals
+            # greedy
+            index = -1
+            numSatisfied = 0
+            newSatisfied = 0
+            for k in range(len(literals)):
+                literal_dict[literals[k].name] = not literals[k].value
+                clauses = updateClauses(clauses, literal_dict)
+
+                # check number satisfied
+                for c in clauses:
+                    if c.evaluate == True:
+                        newSatisfied += 1
+                if newSatisfied >= numSatisfied:
+                    numSatisfied = newSatisfied
+                    index = k
+                literal_dict[literals[k].name] = not literals[k].value
+                clauses = updateClauses(clauses, literal_dict)
+
+            if numSatisfied > bestClause_numSatisfied:
+                bestClause_numSatisfied = numSatisfied
+                bestClause = cindex
+                indexOfBestClause = index
+        literal_dict[clauses[bestClause].literals[indexOfBestClause].name] = not clauses[bestClause].literals[indexOfBestClause].value
+        clauses = updateClauses(clauses, literal_dict)
+    return False
+
 class Literal:
     """Represents a literal"""
 
@@ -139,20 +200,49 @@ if __name__ == '__main__':
 
     clauses = [Cough, Fever, Blood_clot, Stomachache, High_blood_pressure, Headache]
 
-    p = 0.2
-    max_flips = 5
+    p_list = [0.2, 0.8]
+    max_flips_list = [5, 100, 5000]
+    numTrials = 5
 
-    model = WalkSAT(clauses, p, max_flips)
+    # run trials for normal WalkSAT
+    for p in p_list:
+        for max_flips in max_flips_list:
+            for trial in range(numTrials):
+                t0 = time.clock()
+                model = WalkSAT(clauses, p, max_flips)
+                runtime = time.clock() - t0
+                print "***Trial***"
+                print "p = " + str(p) + "; max_flips = " + str(max_flips)
+                print "runtime: " + str(runtime)
 
-    if not model:
-        print "Failure"
-    else:
-        print "diagnosis: patient has"
-        diagnosis = {}
-        for c in model:
-            for lit in c.literals:
-                diagnosis[lit.name] = lit.value
-        print [c for c in diagnosis if diagnosis[c]]
+                if not model:
+                    print "Failure"
+                else:
+                    print "diagnosis:"
+                    diagnosis = {}
+                    for c in model:
+                        for lit in c.literals:
+                            diagnosis[lit.name] = lit.value
+                    print [c for c in diagnosis if diagnosis[c]]
+                print "\n"
 
-    # print function is pretty wonky
-    # print model.__str__()
+    # run WalkSAT_Better trials (greedy)
+    for max_flips in max_flips_list:
+        for trial in range(numTrials):
+            t0 = time.clock()
+            model = WalkSAT_Better(clauses, max_flips)
+            runtime = time.clock() - t0
+            print "***Trial (Improved WalkSAT)***"
+            print "max_flips = " + str(max_flips)
+            print "runtime: " + str(runtime)
+
+            if not model:
+                print "Failure"
+            else:
+                print "diagnosis:"
+                diagnosis = {}
+                for c in model:
+                    for lit in c.literals:
+                        diagnosis[lit.name] = lit.value
+                print [c for c in diagnosis if diagnosis[c]]
+            print "\n"
