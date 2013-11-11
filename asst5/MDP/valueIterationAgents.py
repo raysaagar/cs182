012551553019e3a1b9cb15abb7bfedf974a25ce3
bksim <simpavid@gmail.com>
2013-11-11 00:57:40 -0500

    finished A1

--- asst5/MDP/valueIterationAgents.py
+++ asst5/MDP/valueIterationAgents.py
@@ -31,21 +31,33 @@ class ValueIterationAgent(ValueEstimationAgent):
           mdp.getTransitionStatesAndProbs(state, action)
           mdp.getReward(state, action, nextState)
     """
+    "*** YOUR CODE HERE ***"
     self.mdp = mdp
     self.discount = discount
     self.iterations = iterations
     self.values = util.Counter() # value of each state; a Counter is a dict with default 0
-     
-    "*** YOUR CODE HERE ***"
-        
-             
+    
+    # run for desired number of iterations
+    for i in xrange(iterations):
+      new_values = self.values.copy()
+      for s in mdp.getStates():
+        if not mdp.isTerminal(s):
+          # the commented code works as well
+          #curr_best = float("-inf")
+          #for a in mdp.getPossibleActions(s):
+          #temp_value = sum([p * (mdp.getReward(s, a, s2) + discount*prev[s2]) for s2, p in mdp.getTransitionStatesAndProbs(s, a)])
+          #  if temp_value > curr_best:
+          #    curr_best = temp_value
+          #self.values[s] = curr_best       
+          new_values[s] = max([self.getQValue(s, a) for a in mdp.getPossibleActions(s)])  
+      self.values = new_values
+
   def getValue(self, state):
     """
       Return the value of the state (computed in __init__).
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
+    return self.values[state]
 
   def getQValue(self, state, action):
     """
@@ -55,7 +67,8 @@ class ValueIterationAgent(ValueEstimationAgent):
       necessarily create this quantity and you may have
       to derive it on the fly.
     """
-    util.raiseNotDefined()
+    return sum([p * (self.mdp.getReward(state, action, s2) + self.discount*self.getValue(s2)) 
+      for s2, p in self.mdp.getTransitionStatesAndProbs(state, action)])
 
   def getPolicy(self, state):
     """
@@ -66,7 +79,18 @@ class ValueIterationAgent(ValueEstimationAgent):
       terminal state, you should return None.
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    if self.mdp.isTerminal(state):
+      return None
+    possible_actions = self.mdp.getPossibleActions(state)
+    best_action = None
+    curr_best = float("-inf")
+    for a in possible_actions:
+      temp_value = self.getQValue(state, a)
+      # or equal sign here, doesn't matter (tie)
+      if temp_value >= curr_best:
+        curr_best = temp_value
+        best_action = a
+    return best_action
 
   def getAction(self, state):
     "Returns the policy at the state (no exploration)."
